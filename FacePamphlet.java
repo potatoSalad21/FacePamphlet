b617d8778b19a6f5b2a660c736ea8ea30370f6d0
potatoSalad21 <eeperpeepo@gmail.com>
2025-01-18 16:19:47 +0400

    getting some decomposition goin'

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -11,7 +11,7 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends ConsoleProgram
+public class FacePamphlet extends Program
 					implements FacePamphletConstants {
 
 	/**
@@ -21,6 +21,8 @@ public class FacePamphlet extends ConsoleProgram
 	 */
 	public void init() {
         db = new FacePamphletDatabase();
+        canvas = new FacePamphletCanvas();
+        add(canvas);
         addInteractors();
         addActionListeners();
     }
@@ -30,90 +32,112 @@ public class FacePamphlet extends ConsoleProgram
      * clicked or interactors are used, so you will have to add code
      * to respond to these actions.
      */
-    // TODO decompose this shit
+    // TODO remove println calls
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
         String name = nameField.getText();
 
         switch (cmd) {
         case "Add":
-            if (db.containsProfile(name)) {
-                println("Add: profile for " + name + " already exists: " + db.getProfile(name).toString());
-            } else {
-                FacePamphletProfile profile = new FacePamphletProfile(name);
-                db.addProfile(profile);
-                println("Add: new profile: " + profile.toString());
-            }
-            activeProfile = db.getProfile(name);
-            println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+            handleProfileAdd(name);
             break;
         case "Delete":
-            if (db.containsProfile(name)) {
-                db.deleteProfile(name);
-                println("Delete: profile of " + name + " deleted");
-            } else {
-                println("Delete: profile with the name " + name + " does not exist");
-            }
-            activeProfile = null;
-            println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+            handleProfileDelete(name);
             break;
         case "Lookup":
-            if (db.containsProfile(name)) {
-                FacePamphletProfile profile = db.getProfile(name);
-                println("Lookup: " + profile.toString());
-                activeProfile = profile;
-            } else {
-                println("Lookup: profile with name " + name + " does not exist");
-                activeProfile = null;
-            }
-            println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+            handleProfileLookup(name);
             break;
         case "Change Status":
-            if (activeProfile != null) {
-                println("Status updated to " + statusField.getText());
-                activeProfile.setStatus(statusField.getText());
-                println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
-            } else {
-                println("No active profile; Select the profile first");
-            }
-
+            handleStatusChange();
             break;
         case "Change Picture":
-            if (activeProfile != null) {
-                GImage image;
-                try {
-                    image = new GImage(pictureField.getText());
-                    activeProfile.setImage(image);
-                    println("Change Picture: profile picture changed to: " + pictureField.getText());
-                } catch (ErrorException ex) {
-                    println("Change Picture: Invalid filename");
-                }
-            } else {
-                println("No active profile; Select the profile first");
-            }
-
+            handlePictureChange();
             break;
         case "Add Friend":
-            if (activeProfile != null) {
-                String friendName = newFriendField.getText();
-                if (db.containsProfile(friendName)) {
-                    if (!activeProfile.addFriend(friendName)) {
-                        println("Add Friend: Name is already in the friend list");
-                    } else {
-                        db.getProfile(friendName).addFriend(activeProfile.getName());
-                        println("Add Friend: Added to friend list");
-                    }
-                } else {
-                    println("Add Friend: invalid friend name");
-                }
-            } else {
-                println("Add Friend: No active profile; Select the profile first");
-            }
-
-            println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+            handleAddFriend();
             break;
         }
 	}
+
+    private void handleProfileAdd(String name) {
+        if (db.containsProfile(name)) {
+            println("Add: profile for " + name + " already exists: " + db.getProfile(name).toString());
+        } else {
+            FacePamphletProfile profile = new FacePamphletProfile(name);
+            db.addProfile(profile);
+            println("Add: new profile: " + profile.toString());
+        }
+        activeProfile = db.getProfile(name);
+        println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+    }
+
+    private void handleProfileDelete(String name) {
+        if (db.containsProfile(name)) {
+            db.deleteProfile(name);
+            println("Delete: profile of " + name + " deleted");
+        } else {
+            println("Delete: profile with the name " + name + " does not exist");
+        }
+        activeProfile = null;
+        println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+    }
+
+    private void handleProfileLookup(String name) {
+        if (db.containsProfile(name)) {
+            FacePamphletProfile profile = db.getProfile(name);
+            println("Lookup: " + profile.toString());
+            activeProfile = profile;
+        } else {
+            println("Lookup: profile with name " + name + " does not exist");
+            activeProfile = null;
+        }
+        println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+    }
+
+    private void handleStatusChange() {
+        if (activeProfile != null) {
+            println("Status updated to " + statusField.getText());
+            activeProfile.setStatus(statusField.getText());
+            println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+        } else {
+            println("No active profile; Select the profile first");
+        }
+    }
+
+    private void handlePictureChange() {
+        if (activeProfile != null) {
+            GImage image;
+            try {
+                image = new GImage(pictureField.getText());
+                activeProfile.setImage(image);
+                println("Change Picture: profile picture changed to: " + pictureField.getText());
+            } catch (ErrorException ex) {
+                println("Change Picture: Invalid filename");
+            }
+        } else {
+            println("No active profile; Select the profile first");
+        }
+    }
+
+    private void handleAddFriend() {
+        if (activeProfile != null) {
+            String friendName = newFriendField.getText();
+            if (db.containsProfile(friendName) && activeProfile.getName() != friendName) {
+                if (!activeProfile.addFriend(friendName)) {
+                    println("Add Friend: Name is already in the friend list");
+                } else {
+                    db.getProfile(friendName).addFriend(activeProfile.getName());
+                    println("Add Friend: Added to friend list");
+                }
+            } else {
+                println("Add Friend: invalid friend name");
+            }
+        } else {
+            println("Add Friend: No active profile; Select the profile first");
+        }
+
+        println(activeProfile != null ? ":: > Current profile: " + activeProfile.toString() : ":: > No current profile");
+    }
 
     // adds all the interactors for the GUI
     private void addInteractors() {
@@ -159,11 +183,13 @@ public class FacePamphlet extends ConsoleProgram
     /* INSTANCE VARIABLES */
 
     // GUI components
-    JTextField nameField;
-    JTextField statusField;
-    JTextField pictureField;
-    JTextField newFriendField;
+    private JTextField nameField;
+    private JTextField statusField;
+    private JTextField pictureField;
+    private JTextField newFriendField;
 
-    FacePamphletDatabase db;
-    FacePamphletProfile activeProfile;
+    private FacePamphletCanvas canvas;
+
+    private FacePamphletDatabase db;
+    private FacePamphletProfile activeProfile;
 }
